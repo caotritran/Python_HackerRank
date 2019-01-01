240384fc4d589b97b71bc91c521527e515b2c176
caotritran <caotritran.14@gmail.com>
2019-01-01 08:06:09 +0700

    Update cal_cangchi.py

--- cal_cangchi.py
+++ cal_cangchi.py
@@ -1,13 +1,13 @@
-#Tinh can chi
 
-CANG = ['Canh', 'Tan', 'Nham', 'Quy', 'Giap', 'At', 'Binh', 'Dinh', 'Mau', 'Ky']
+
+CAN = ['Canh', 'Tan', 'Nham', 'Quy', 'Giap', 'At', 'Binh', 'Dinh', 'Mau', 'Ky']
 CHI = ['Than', 'Dau', 'Tuat', 'Hoi', 'Ty', 'Suu', 'Dan', 'Meo', 'Thin', 'Ty', 'Ngo', 'Mui', ]
 
 year = int(input("Nhap so nam can tinh can chi: "))
-_cang = year%10
+_can = year%10
 _chi = year%12
 
-cangchi = CANG[_cang] + " " + CHI[_chi]
+canchi = CAN[_can] + " " + CHI[_chi]
 
 
-print('can chi nam {} se la: {}'.format(year, cangchi))
+print('can chi nam {} se la: {}'.format(year, canchi))
