7142be08a72acf9e6d375e656864b80e06e898c8
caotritran <caotritran.14@gmail.com>
2019-01-01 09:34:48 +0700

    Create textwrap.py

---
+++ textwrap.py
@@ -0,0 +1,57 @@
+'''
+You are given a string  and width . 
+Your task is to wrap the string into a paragraph of width .
+
+Input Format
+
+The first line contains a string, . 
+The second line contains the width, .
+
+Constraints
+
+Output Format
+
+Print the text wrapped paragraph.
+
+Sample Input 0
+
+ABCDEFGHIJKLIMNOQRSTUVWXYZ
+4
+Sample Output 0
+
+ABCD
+EFGH
+IJKL
+IMNO
+QRST
+UVWX
+YZ
+'''
+
+import textwrap
+
+def wrap(string, max_width):
+    li = textwrap.fill(string, max_width)
+
+    return li
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
+    
+### Hoac cach giai khac ###
+'''
+import textwrap
+
+def wrap(string, max_width):
+    li = textwrap.wrap(string, max_width)
+    return li
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+
+    for i in result:
+        print(i)
+'''
